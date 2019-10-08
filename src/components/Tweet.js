2d0bc4127a60e4b2b242ddc49321495f237437a3
Simon <scwong93@gmail.com>
2019-10-08 11:37:24 -0700

    render ui

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { formatTweet } from '../utils/helpers'
+import { formatTweet, formatDate } from '../utils/helpers'
+import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
+import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
+import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
 
 class Tweet extends Component {
+    handleLike = (e) => {
+        e.preventDefault()
+
+    }
+    toParent = (e, id) => {
+        e.preventDefault()
+
+    }
+
     render() {
+        const { tweet } = this.props
+
+        if (tweet === null) {
+            return <p>This Tweet does not exists</p>
+        }
+
+        const {
+            name, avatar, timestamp, text, hasLiked, likes, replies, parent
+        } = tweet
+
         return (
             <div className='tweet'>
-
+                <img
+                    src={avatar}
+                    alt={`Avatar of ${name}`}
+                />
+                <div className='tweet-info'>
+                    <div>
+                        <span>{name}</span>
+                        <div>{formatDate(timestamp)}</div>
+                        {parent && (
+                            <button className='replying-to' onclick={(e) => this.toParent(e, parent.id)}>
+                                Replying to @{parent.author}
+                            </button>
+                        )}
+                        <p>{text}</p>
+                    </div>
+                    <div className='tweet-icons'>
+                        <TiArrowBackOutline className='tweet-icon' />
+                        <span>{replies !== 0 && replies}</span>
+                        <button className='heart-button' onclick={this.handleLike}>
+                            {hasLiked === true 
+                                ? <TiHeartFullOutline color='#e0245e' className='tweet-icon'/>
+                                : <TiHeartOutline className='tweet-icon' />}
+                        </button>
+                        <span>{likes !== 0 && likes}</span>
+                    </div>
+                </div>
             </div>
         )
     }
@@ -14,10 +61,11 @@ class Tweet extends Component {
 
 function mapStateToProps ({ authedUser, users, tweets }, { id }) {
     const tweet = tweets[id]
+    const parentTweet = tweet ? tweets[tweet.replyingTo] : null
 
     return {
         authedUser,
-        tweet: formatTweet(tweet, users[tweet.author], authedUser)
+        tweet: tweet ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet) : null
     }
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import authedUser from './authedUser';
 import user from './users';
 import tweets from './tweets';
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
     authedUser,
     user,
-    tweets
+    tweets,
+    loadingBar: loadingBarReducer
 })
