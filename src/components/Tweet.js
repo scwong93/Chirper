d355419a8b45ae69b9f9755888d176ea7ca07752
Simon <scwong93@gmail.com>
2019-10-08 16:54:36 -0700

    handle like toggling

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,11 +4,20 @@ import { formatTweet, formatDate } from '../utils/helpers'
 import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
 import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
 import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
+import { handleToggleTweet } from '../actions/tweets'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
     handleLike = (e) => {
         e.preventDefault()
 
+        const { dispatch, tweet, authedUser } = this.props
+
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }))
     }
     toParent = (e, id) => {
         e.preventDefault()
